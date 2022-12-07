94f1e38871a954a025f43edc0504c5e1131355bd
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-12-07 18:44:09 +0100

    commit de l'exercice 2 testé - méthodes statiques

---
+++ ex_02.php
@@ -0,0 +1,109 @@
+<?php
+
+interface iCars
+{
+    public function getPrice();
+
+    public function getWeight();
+
+    public function mineIsBigger($obj);
+}
+
+
+class BMW implements iCars
+{
+    private $_weight;
+    private $_price;
+
+    public function __construct($price,$weight=4242)
+    {
+        $this->$_weight=$weight;
+        $this->$_price=$price;
+    }
+
+    static function lessExpansive()
+    {
+        return 15000;
+    }
+
+    public function getPrice()
+    {
+        return $this->_price;
+    }
+
+    public function getWeight()
+    {
+        return $this->_weight;
+    }
+
+    public function mineIsBigger($obj)
+    {
+        if(is_a($obj,"Toyota"))
+        {
+            echo "Minbe is bigger";
+        }
+        elseif (is_a($obj,"Smart")) 
+        {
+            echo "Mine is way bigger !";
+        }
+        elseif(is_a($obj,"Velib"))
+        {
+            echo "LOL";
+        }
+        else
+        {
+            echo "Show me !";
+        }
+    }
+}
+
+class Suzuki implements iCars
+{
+    private $_weight;
+    private $_price;
+
+    public function __construct($price,$weight=4242)
+    {
+        $this->$_weight=$weight;
+        $this->$_price=$price;
+    }
+
+    static function lessExpansive()
+    {
+        return 5000;
+    }
+
+    public function getPrice()
+    {
+        return $this->_price;
+    }
+
+    public function getWeight()
+    {
+        return $this->_weight;
+    }
+
+    public function mineIsBigger($obj)
+    {
+        if(is_a($obj,"Toyota"))
+        {
+            echo "Minbe is bigger";
+        }
+        elseif (is_a($obj,"Smart")) 
+        {
+            echo "Mine is way bigger !";
+        }
+        elseif(is_a($obj,"Velib"))
+        {
+            echo "LOL";
+        }
+        else
+        {
+            echo "Show me !";
+        }
+    }
+}
+/*
+echo Suzuki::lessExpansive();
+echo BMW::lessExpansive();
+*/
