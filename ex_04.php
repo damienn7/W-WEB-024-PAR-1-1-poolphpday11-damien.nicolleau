ac7f134f1d4e3b5cbed38f839a66889d0523ef1e
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-12-07 19:46:07 +0100

    commit de l'exercice 4 - fonction static avec print différent selon l'objet

---
+++ ex_04.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Imperium{
+class Soldier
+{
+
+    private $hp;
+    private $attack;
+    private $name;
+
+    public function __construct($name,$attack=12,$hp=50)
+    {
+        $this->hp=$hp;
+        $this->attack=$attack;
+        $this->name=$name;
+    }
+
+    public function getHP()
+    {
+        return $this->hp;
+    }
+
+    public function setHp($hp)
+    {
+        $this->hp=$hp;
+    }
+
+    public function getAttack()
+    {
+        return $this->attack;
+    }
+
+    public function setAttack($attack)
+    {
+        $this->attack=$attack;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name=$name;
+    }
+
+    public function doDamage($obj)
+    {
+        if(is_a($obj,"Chaos\Soldier") || is_a($obj,"Imperium\Soldier"))
+        {
+            $obj->setHp($obj->getHp()-$this->getAttack());
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public function __toString()
+    {
+        return "Gessart the Imperium Space Marine : ".$this->getHp()." HP.";
+    }
+}
+}
+
+namespace Chaos{
+
+class Soldier
+{
+    
+    private $hp;
+    private $attack;
+    private $name;
+
+
+    public function __construct($name,$attack=12,$hp=70)
+    {
+        $this->hp=$hp;
+        $this->attack=$attack;
+        $this->name=$name;
+    }
+
+    public function getHP()
+    {
+        return $this->hp;
+    }
+
+    public function setHp($hp)
+    {
+        $this->hp=$hp;
+    }
+
+    public function getAttack()
+    {
+        return $this->attack;
+    }
+
+    public function setAttack($attack)
+    {
+        $this->attack=$attack;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name=$name;
+    }
+
+    public function doDamage(object $obj)
+    {
+        if(is_a($obj,"Imperium\Soldier") || is_a($obj,"Chaos\Soldier"))
+        {
+            $obj->setHp($obj->getHp()-$this->getAttack());
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public function __toString()
+    {
+        return "Gessart the Imperium Space Marine : ".$this->getHp()." HP.";
+    }
+}
+}
+
+
+namespace {
+
+    class Scanner
+    {
+        static function scan($obj)
+        {
+            if(gettype($obj)==="object")
+            {
+
+            
+            if(is_a($obj,"Imperium\Soldier"))
+            {
+                echo "Praise be, Emperor, Lord.\n";
+            }
+            
+            if(is_a($obj,"Chaos\Soldier"))
+            {
+                echo "Xenos spotted.\n";
+            }
+
+        }
+        }
+    }
+
+/*
+$spaceMarine = new Imperium\Soldier(" Gessart ") ;
+$chaosSpaceMarine = new Chaos\Soldier(" Ruphen ") ;
+echo $spaceMarine . "\n";
+echo $chaosSpaceMarine . "\n";
+$spaceMarine->doDamage( $chaosSpaceMarine ) ;
+echo $spaceMarine . "\n";
+echo $chaosSpaceMarine . "\n";
+
+Scanner::scan($spaceMarine);
+*/
+}
+
